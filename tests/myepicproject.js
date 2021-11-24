20cda91f7e0354f48fdf267fa97dcf013b24d74f
Kriptikz <cruzxkriptikz@gmail.com>
2021-11-24 19:12:09 +0100

    Updated test js to create a tx for the start_stuff_off fn with accounts for params. Then read the data from that account using its public key.

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,37 @@
 const anchor = require('@project-serum/anchor');
 
+// Need the system program 
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
   console.log("Starting test...")
 
-  anchor.setProvider(anchor.Provider.env());
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  // * I'm curious why we created the provider variable, maybe we will update it in the future, or we may need to read it.
+  // * Looks like its used when creating the tx so we don't have to type anchor.Provider.env() every time.
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Now our start_stuff_off function requires params for the context.
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('Gif Count', account.totalGifs.toString())
 }
 
 const runMain = async () => {
